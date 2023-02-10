d8188c25590f88904deb0647f9440a895eb3988b
zk524 <20262010007@fudan.edu.cn>
2023-02-10 12:45:09 +0800

    clearify

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 import React, { useState, useRef, useEffect } from 'react'
 import Linkify from 'react-linkify'
-import UserInput from './UserInput'
-import FileIcon from './components/FileIcon'
+import EmojiConvertor from 'emoji-js'
+import EmojiIcon from './components/EmojiIcon'
+import SendIcon from './components/SendIcon'
 import chatIconUrl from './assets/chat-icon.svg'
+import emojiData from './assets/emojiData'
 import incomingMessageSound from './assets/notification.mp3'
 
 const notifyAudio = new Audio(incomingMessageSound)
+const emojiConvertor = new EmojiConvertor()
+emojiConvertor.init_env()
 
 export default () => {
   const [messageList, setMessageList] = useState([
     { type: 'text', author: 'wallet', data: { text: 'Welcome to Fake Wallet!' } },
   ])
+  const [state, setState] = useState({ inputActive: false, inputHasText: false, emojiPickerIsOpen: false })
   const messageRef = useRef(null)
+  const inputRef = useRef(null)
 
   useEffect(() => {
     const last = messageList.slice(-1)[0]
@@ -33,7 +39,7 @@ export default () => {
         </div>
         <div className="sc-message-list" ref={messageRef}>
           {messageList.map((message, i) => (
-            <div className="sc-message" key={i}>
+            <div className="sc-message" key={'message' + i}>
               <div className={`sc-message--content ${message.author === 'me' ? 'sent' : 'received'}`}>
                 <div className="sc-message--avatar" style={{ backgroundImage: `url(${chatIconUrl})` }}></div>
                 {message.type === 'text' && (
@@ -42,32 +48,78 @@ export default () => {
                   </div>
                 )}
                 {message.type === 'emoji' && <div className="sc-message--emoji">{message.data.emoji}</div>}
-                {message.type === 'file' && (
-                  <a className="sc-message--file" href={message.data.url} download={message.data.fileName}>
-                    <FileIcon />
-                    <p>{message.data.fileName}</p>
-                  </a>
-                )}
               </div>
             </div>
           ))}
         </div>
-        <UserInput
-          onSubmit={(message) => setMessageList([...messageList, message])}
-          onFilesSelected={(fileList) => {
-            setMessageList([
-              ...messageList,
-              {
-                type: 'file',
-                author: 'me',
-                data: {
-                  url: window.URL.createObjectURL(fileList[0]),
-                  fileName: fileList[0].name,
-                },
-              },
-            ])
-          }}
-        />
+
+        <form className={`sc-user-input ${state.inputActive ? 'active' : ''}`}>
+          <div
+            role="button"
+            tabIndex="0"
+            onFocus={() => setState({ ...state, inputActive: true })}
+            onBlur={() => setState({ ...state, inputActive: false })}
+            ref={inputRef}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter' && !e.shiftKey) {
+                e.preventDefault()
+                const text = inputRef.current.textContent
+                if (text && text.length > 0) {
+                  setMessageList([...messageList, { author: 'me', type: 'text', data: { text } }])
+                  inputRef.current.innerHTML = ''
+                }
+              }
+            }}
+            onKeyUp={(e) =>
+              setState({ ...state, inputHasText: e.target.innerHTML.length !== 0 && e.target.innerText !== '\n' })
+            }
+            contentEditable="true"
+            placeholder="Write a reply..."
+            className="sc-user-input--text"
+          ></div>
+          <div className="sc-user-input--buttons">
+            <div className="sc-user-input--button">
+              <EmojiIcon
+                onClick={(e) => {
+                  e.preventDefault()
+                  setState({ ...state, emojiPickerIsOpen: !state.emojiPickerIsOpen })
+                }}
+                isActive={state.emojiPickerIsOpen}
+                tooltip={
+                  <div className="sc-popup-window">
+                    <div className={`sc-popup-window--cointainer ${state.emojiPickerIsOpen ? '' : 'closed'}`}>
+                      <div className="sc-emoji-picker">
+                        <div className="sc-emoji-picker--category">
+                          {emojiData.map(({ emoji }) => (
+                            <span
+                              className="sc-emoji-picker--emoji"
+                              key={emoji}
+                              onClick={() => {
+                                setState({ ...state, emojiPickerIsOpen: false })
+                                if (state.inputHasText) inputRef.current.innerHTML += emoji
+                                else setMessageList([...messageList, { author: 'me', type: 'emoji', data: { emoji } }])
+                              }}
+                            >
+                              {emoji}
+                            </span>
+                          ))}
+                        </div>
+                        )
+                      </div>
+                    </div>
+                  </div>
+                }
+              />
+            </div>
+            <div className="sc-user-input--button">
+              <SendIcon
+                onClick={() => {
+                  console.log(1)
+                }}
+              />
+            </div>
+          </div>
+        </form>
       </div>
     </div>
   )
