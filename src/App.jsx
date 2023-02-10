a9e484c0559d0fcbbbe458ca0bbff3e68343a7ba
zk524 <20262010007@fudan.edu.cn>
2023-02-10 12:29:31 +0800

    clear

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useRef, useEffect } from 'react'
-import Message from './Message'
+import Linkify from 'react-linkify'
 import UserInput from './UserInput'
+import FileIcon from './components/FileIcon'
+import chatIconUrl from './assets/chat-icon.svg'
 import incomingMessageSound from './assets/notification.mp3'
 
 const notifyAudio = new Audio(incomingMessageSound)
@@ -30,9 +32,25 @@ export default () => {
           <div className="sc-header--team-name"> {'Fake Wallet'} </div>
         </div>
         <div className="sc-message-list" ref={messageRef}>
-          {messageList.map((message, i) => {
-            return <Message message={message} key={i} />
-          })}
+          {messageList.map((message, i) => (
+            <div className="sc-message" key={i}>
+              <div className={`sc-message--content ${message.author === 'me' ? 'sent' : 'received'}`}>
+                <div className="sc-message--avatar" style={{ backgroundImage: `url(${chatIconUrl})` }}></div>
+                {message.type === 'text' && (
+                  <div className="sc-message--text">
+                    {<Linkify properties={{ target: '_blank' }}>{message.data.text}</Linkify>}
+                  </div>
+                )}
+                {message.type === 'emoji' && <div className="sc-message--emoji">{message.data.emoji}</div>}
+                {message.type === 'file' && (
+                  <a className="sc-message--file" href={message.data.url} download={message.data.fileName}>
+                    <FileIcon />
+                    <p>{message.data.fileName}</p>
+                  </a>
+                )}
+              </div>
+            </div>
+          ))}
         </div>
         <UserInput
           onSubmit={(message) => setMessageList([...messageList, message])}
